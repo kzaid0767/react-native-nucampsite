fe7d47971a21d51276fe6293573f82363af76d87
Kassim Zaid <kzaid0767@gmail.com>
2022-05-07 13:12:33 -0400

    Components Part 1

---
+++ components/DirectoryComponent.js
@@ -0,0 +1,26 @@
+import  React from "react";
+import { FlatList } from "react-native";            //wraps all list items
+import { ListItem } from "react-native-elements";     //wraps each indvidual item
+
+function Directory(props){
+    
+    const renderDirectoryItem = ({item}) => {
+        return (
+            <ListItem
+                title={item.name}
+                subtitle={item.description}
+                leftAvatar={{source: require('./images/react-lake.jpg')}}
+            />   
+        )
+    }
+
+    return(
+        <FlatList
+            data={props.campsites}
+            renderItem={renderDirectoryItem}
+            keyExtractor = {item => item.id.toString()} //expects string so convert id to string
+        />
+    )
+}
+
+export default Directory
